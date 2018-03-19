0831fdd5ebb2aa4543ec3a255eed77a7d0fd6208
up201504024 <36534528+up201504024@users.noreply.github.com>
2018-03-19 21:31:03 +0000

    Add files via upload

--- leitura.c
+++ leitura.c
@@ -2,19 +2,20 @@
 #include <stdlib.h> 
 #include <string.h>
 
-
 int main(){
 	
 	FILE * file;
+	int NELEMENTOSMATRIX = 12;
+	const char espaco[2]=" ";
 	char conteudo[500]={};
-	char *str;
+	char *valor;
 	int i=0;
 	int j=0;
 	int k=0;
-	int vetor[12][12]={0};
-	char *as;
+	int n=0;
+	int v[12][12]={0};
+	int vetor[500];
 	
-	as = " ";
 	
 	file=fopen("teste.txt","r");
 	
@@ -30,123 +31,41 @@ int main(){
 		
 		printf("\n confirmaçao inicial\n");
 	
-	
+	valor=strtok(conteudo, espaco);
+		
+		while(valor != NULL){
+			
+			
+			vetor[n]=atoi(valor);
+			printf(" %d", vetor[n]);
+			valor=strtok(NULL,espaco);
+			//printf("%d\n", vetor[n]);
+			n++;
+			
+		}
+		
+		//printf("\n%d",n);
+		
+	printf("\nULTIMA FASE:\n");	
 	
 	i=0; //linha
-	j=0; //coluna
-	k=0; //nºelemento
+	j=0; //coluna 
+	k=0; //percorrer vetor
 	
-	
-	while( conteudo[k] != '\0'){
-	
-		j=0;
-		
-		//printf("\nteste");	
-		
-	
-	while(conteudo[k]!= '\n'){
-
-
-		//str = strtok(NULL, " ");
-		
-		str=strtok(conteudo,as);
-		
-		printf("%d", atoi(str));
-		vetor[i][j]=atoi(str);	
-		j++;	
-		k++;
-		str = strtok(NULL, " ");
-		printf(" teste %d", vetor[i][j]); 
-	
-}
-	i++;
-	k++;
-
-}	
-	/*
-		
-		j = 0;
-		//fgets(conteudo, 500, stdin);
-		printf("teste1");
-		str = strtok(conteudo, "\n");
-		printf(" teste2 ");
-		
-		for(i=0;str != NULL;i++){
-			for(j=0;strcmp(str,as)==0;j++){
-				vetor[i][j] = atoi(str);
-				printf("%d ",vetor[i][j]);
-				str = strtok(NULL, " ");
+	for(i=0;i<(NELEMENTOSMATRIX-1);i++){
+			
+			for(j=0;j<(NELEMENTOSMATRIX-1);j++){
+				
+				v[i][j]=vetor[k];
+				printf(" %d", v[i][j]);
+				k++;
 			}
-}
-}	
-	*/
-	/*
-	while((conteudo[i] = fgetc(file)) != EOF){
+			}
 		
-		
-		printf("%c", conteudo[i]);
-		i++;
-		}
-		
-	
-	
-		i=0;
-		j=0;
-		aux=0;
-		
-
-		while(conteudo[i] != EOF){
-		
-		//escreve se nao estiver nada no vetor
-		if(conteudo[i] != ' '){		
-			
-			i++;
-			aux++;
-		}
-		//não é necessario juntar numeros
-		else if (conteudo[i] == ' ' && aux==1){
-			
-			
-			//vetor[j]=conteudo[i]; 
-			i++;
-			j++;
-			aux=0;
-			continue;
-			
-		
-		}
-		//caso algarismo 2 numeros
-		else if(conteudo[i] == ' ' && aux==2){
-			
-			//strcpy(vetor[j], conteudo[i-1]);
-			//strcat(vetor[j], conteudo[i]);
-			i++;
-			j++;
-			aux=0;
-			
-			//printf("%c", vetor[j]);
-			
-		}
-		
-	}
-		//printf("%c", vetor[i]);
-		
-		
-		
-*/	
+	printf("\n teste %d", v[1][0]);
+	printf("\n s %d", vetor[12]);
 
 	fclose(file);
 	return 0;
 	
 }
-
-
-
-/*
-int main () {
-	
-	
-	
-	
-}
-*/ 
